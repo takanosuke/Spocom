c9db7d2aca22c0da41529952847f8f3901003c6c
shinnosuke.takai <s.takanosuke9718@gmail.com>
2018-10-22 17:16:14 +0000

    deviseで簡単なログイン機能実装

---
+++ app/controllers/video_controller.rb
@@ -0,0 +1,12 @@
+class VideoController < ApplicationController
+  def topPage
+  end
+  def selects
+  end
+  def boughtList
+  end
+  def watch
+  end
+  def upload
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'video#topPage'
+  #devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
 end
