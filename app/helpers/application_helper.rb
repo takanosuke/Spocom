fc68bd19f7850e7ef18b26c096e9efce43ea3020
shinnosuke.takai <s.takanosuke9718@gmail.com>
2019-02-13 00:52:50 +0900

    CSS周りのリファクタリング 軽いレイアウト調整

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def form_error_messages(model)
+    return "" if model.errors.empty?
+    html = "<div id='error_explanation' class='alert alert-danger'><ul>"
+    messages = model.errors.full_messages.each do |message|
+      html += "<li>#{message}</li>"
+    end        
+    html += "</ul></div>"
+    html.html_safe
+  end
 end
